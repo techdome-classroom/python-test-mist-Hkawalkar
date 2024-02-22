e1ddb5f6ee91bbedd0dbd895f72b3382752d5015
Hkawalkar <160535129+Hkawalkar@users.noreply.github.com>
2024-02-22 13:46:39 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        class Solution(object):
+    def isValid(str) -> bool:
+        stack = []
+        mapping = {'(':')','[':']','{':'}'}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+                else:
+                    stack.append(char)
+                    return not stack
+        
+    
+
+
+
+  
+
+
     
 
 
